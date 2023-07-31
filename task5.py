433e8fdfd277f7d8e4266dcc0c53878564548ae4
deema-19 <deemajenana12@gmail.com>
2023-07-31 21:55:33 +0300

    first commit for task5

---
+++ task5.py
@@ -0,0 +1,126 @@
+
+# name=input( 'Name:  ')
+# print('"Name:'+(name)+"\"")
+# age=int(input( 'age:  '))
+# print('"Age:'+(age)+"\"")
+# address=input( 'adrdress: ')
+# print('"address:'+(address)+"\"")
+
+
+
+# name='ali'
+# age=22
+# # street=142
+# city='gaza'
+# country='palestine'
+# new_age=age-5
+# output=f'hello {({name})} your age is {new_age} years ole , your address is {street,city,countr }'
+# print(output)
+
+
+# print(type(name))
+# print(type(age))
+# print(type(street))
+# print(type(city))
+# print(type(country))
+
+
+# output=f'hello \'{name}\', how are you? \ """ your age is \"{new_age}\"+and your country is : {country}'
+# print(output)
+
+
+# output=f'hello \'{name}\', how are you? \ """ your age is \"{new_age}\"+and your city is : {city}'
+# print(output)
+
+
+# name = 'Doaa Reem'
+# print("First Letter Is", '"' + name[0] + '"')
+# print("First Letter Is", '"' + name[2] + '"')
+# print("First Letter Is", '"' + name[-1] + '"')
+
+
+# name = 'Doaa Reem'
+# print(name[6:])
+# print(name[0:4])
+# print(name[:])
+# x=name[2:3].upper()+name[3:7]
+# print(x)
+
+
+# name = "$&$&Mohammed$&$&"
+# print(name.strip('$&'))
+
+
+
+# msg = "I %7 Python And Although I %7 GSG with Zakaria"
+# print (msg.replace('%7',"love"))
+
+
+# name1 = "gaza  sky geeks"
+# name2 = "love pythone"
+# print(name1.capitalize() )
+# print(name2.title())
+
+
+# num1 = "4"
+# num2 = "56"
+# num3 = "963"
+# num4 = "385"
+# num5 = "8719"
+# num6 = "87190"
+# print(num1.zfill(5))
+# print(num2.zfill(5))
+# print(num3.zfill(5))
+# print(num4.zfill(5))
+# print(num5.zfill(5))
+# print(num6.zfill(5))
+
+# first_name = "Hadeel"
+# print("*" * 11 + first_name)
+# print("*" * 11 + first_name + "*" * 11)
+# print(first_name + "*" * 11)
+
+# name_one = "HaLA"
+# name_two = "shaHD"
+# print(name_one[0].lower()+name_one[1].upper()+name_one[2].lower()+name_one[3].lower())
+# print(name_two[0].upper()+name_two[1].upper()+name_two[2].upper()+name_two[3].lower()+name_two[4].lower())
+# print(name_one.lower())
+# print(name_two.upper())
+
+# name_one = "HaLA"
+# name_two = "shaHD"
+# if name_one.isupper():
+#     print("yes")
+# else:
+#     print("no not the all sentence")
+#  if name_two.isupper():
+#         print("yes")
+#     else:
+#         print("no not the all sentence")
+
+
+# if name_one.find("S"):
+#     print("yes")
+# else:
+#     print("no not the all sentence")
+# if name_two.find('HD'):
+#         print("yes")
+# else:
+#         print("no not the all sentence")
+
+
+# msg = "I Love Python And Although I Love GSG with Zakaria"
+# love_count = msg.count("Love")
+# t_count = msg.count("t")
+# print("love",{love_count} )
+# print( "t",{t_count} )
+
+
+# name = "Zakaria"
+# r_position = name.index("r")
+# print(r_position)
+
+
+# msg = "I %7 Python And Although I %7 GSG with Zakaria"
+# new_msg = msg.replace("%7", "Love", 1)
+# print(new_msg)
